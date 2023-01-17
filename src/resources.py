b63d6c678a59122bb604dea9600eb9fe2521201b
Schallbert <schal@mailbox.org>
2023-01-17 21:36:34 +0100

    - add xbm icon for running on linux - update resources file to reflect OS differences

--- src/resources.py
+++ src/resources.py
@@ -1,13 +1,11 @@
-from src import assets
-import importlib.resources as pkg_resources
-from tkinter.messagebox import showerror
+import sys
 
-try:
-    with pkg_resources.path(assets, 'qruwu.ico') as p_icon:
-        app_icon_path = p_icon
-    with pkg_resources.path(assets, 'qruwu.png') as p_image:
-        app_image_path = p_image
-    with pkg_resources.path(assets, 'persistence.dat') as p_persistence:
-        app_persistence_path = p_persistence
-except FileNotFoundError:
-    showerror(title='Asset file not found', message='Could not locate file(s) under "src/assets"')
+asset_path = 'src/assets'
+
+if sys.platform.startswith('linux'):  # could be "linux", "linux2", "linux3", ...
+    app_icon_path = asset_path + '/qruwu.xbm'
+elif sys.platform.startswith('win32'):
+    app_icon_path = asset_path + '/qruwu.ico'
+
+app_image_path = asset_path + '/qruwu.png'
+app_persistence_path = asset_path + '/persistence.dat'
