fd15eb936ed93a94cc53a344ddecbbe730de7bc2
Schallbert <schal@mailbox.org>
2023-02-04 20:49:12 +0100

    write new path algo and add unit testing

--- src/platform/scan_qr.py
+++ src/platform/scan_qr.py
@@ -5,46 +5,107 @@ class Position:
 
 
 class LineSegment:
-    def __init__(self, x, y, position):
-        self.x = x
-        self.y = y
+    def __init__(self, x_length, y_length, position):
+        self.x_length = x_length
+        self.y_length = y_length
         self.position = position
 
 
+class QrValueTable:
+    def __init__(self):
+        self.table = {}
+        self.size = 0
+
+    def set_qr(self, qr):
+        self.size = qr.get_size()
+        for y in range(self.size):
+            for x in range(self.size):
+                self.table[x, y] = qr.get_module(x, y)
+
+
 class ScanQr:
-    def __init__(self, qr):
-        self._size = qr.get_size()
+    def __init__(self, qr_value_table):
+        self._qr_todo = qr_value_table
 
-        self._qr_todo = {}
-        for x in range(self._size):
-            for y in range(self._size):
-                self._qr_todo[x][y] = qr.get_module(x, y)
+    def get_vectors(self):
+        line_list = []
+        for line in range(self._qr_todo.size):
+            line_list += self._get_line_left_to_right(line)
+            # if line + 1 < self._qr_todo.size:
+            #    line_list += self._get_line_right_to_left(line + 1)
+        return line_list
 
-    def _get_line(self, row):
+    def _get_line_left_to_right(self, row):
+        """This algorithm walks through a line of the QR-code left to right and line by line to construct vectors
+        of coherent bits that are True. If the bit below the currently targeted bit is also True, then a vertical
+        line is created. Else, a horizontal line is created."""
         vectors = []
         position = Position(0, row)
-        x_length = 1
+        x_length = 0
         y_length = 1
 
-        while position.x < self._size:
-            state = self._qr_todo[position.x][row]
-            if state:
-                while self._qr_todo[x_length][row + y_length]:
+        if row > self._qr_todo.size:
+            return
+
+        while position.x < self._qr_todo.size:
+            if self._qr_todo.table[row, position.x]:
+                while self._qr_todo.table[row + y_length, position.x]:
                     y_length = y_length + 1
+                    if row + y_length >= self._qr_todo.size:
+                        break
                 if y_length > 1:
-                    segment = LineSegment(0, y_length, position)
+                    segment = LineSegment(0, y_length, Position(position.x, position.y))
                     vectors.append(segment)
-                    self._clear(segment)
+                    self._clear_todo(segment)
+                    y_length = 1
                 else:
                     x_length = x_length + 1
-            else:
-                segment = LineSegment(x_length, 0, position)
+            elif x_length > 0:
+                segment = LineSegment(x_length, 0, Position(position.x - x_length, position.y))
                 vectors.append(segment)
-                self._clear(segment)
+                self._clear_todo(segment)
+                x_length = 0
             position.x = position.x + 1
+        return vectors
 
-    def _get_column(self):
-        pass
+    def _get_line_right_to_left(self, row):
+        """This algorithm walks through a line of the QR-code right to left and line by line to construct vectors
+        of coherent bits that are True. If the bit below the currently targeted bit is also True, then a vertical
+        line is created. Else, a horizontal line is created."""
+        vectors = []
+        position = Position(self._qr_todo.size, row)
+        start_position = Position(self._qr_todo.size, row)
+        x_length = 0
+        y_length = 1
 
-    def _clear(self, segment):
-        pass
+        if row > self._qr_todo.size:
+            return
+
+        while position.x >= 0:
+            if self._qr_todo.table[position.x][row]:
+                while self._qr_todo.table[x_length][row + y_length]:
+                    y_length = y_length + 1
+                if y_length > 1:
+                    segment = LineSegment(0, y_length, start_position)
+                    vectors.append(segment)
+                    self._clear_todo(segment)
+                    y_length = 1
+                else:
+                    x_length = x_length + 1
+            elif x_length > 0:
+                segment = LineSegment(-x_length, 0, start_position)
+                vectors.append(segment)
+                self._clear_todo(segment)
+                x_length = 0
+            else:
+                start_position.x = position.x - 1
+            position.x = position.x - 1
+        return vectors
+
+    def _clear_todo(self, segment):
+        if segment.x_length == 0:
+            for y in range(segment.y_length):
+                self._qr_todo.table[segment.position.y + y, segment.position.x] = False
+        elif segment.y_length == 0:
+            for x in range(segment.x_length):
+                self._qr_todo.table[segment.position.y, segment.position.x + x] = False
---
+++ test/test_scan_qr.py
@@ -0,0 +1,99 @@
+import unittest
+from unittest.mock import MagicMock
+
+from qrcodegen import QrCode
+from src.platform.scan_qr import QrValueTable, ScanQr
+
+
+class TestQrValueTable(unittest.TestCase):
+    def setUp(self):
+        self.mock_qr = QrCode.encode_text("schallbert.de", QrCode.Ecc.MEDIUM)
+        self.mock_qr.get_module = MagicMock()
+        self.mock_qr.get_size = MagicMock()
+
+    def test_qrvaluetable_setqr_size_matches(self):
+        self.mock_qr.get_module.return_value = True
+        self.mock_qr.get_size.return_value = 21
+        table = QrValueTable()
+        table.set_qr(self.mock_qr)
+        self.assertEqual(21, table.size)
+
+    def test_qrvaluetable_setqr_table_matches_size(self):
+        self.mock_qr.get_module.return_value = True
+        self.mock_qr.get_size.return_value = 21
+        table = QrValueTable()
+        table.set_qr(self.mock_qr)
+        self.assertEqual(True, table.table[20, 20])
+
+    def test_qrvaluetable_setqr_indexoutofrange_throws(self):
+        self.mock_qr.get_module.return_value = False
+        self.mock_qr.get_size.return_value = 21
+        table = QrValueTable()
+        table.set_qr(self.mock_qr)
+        self.assertRaises(Exception, table.table, [21, 21])
+
+
+class TestScanQr(unittest.TestCase):
+    def setUp(self):
+        # 1 0 1 1 0
+        # 0 1 1 0 0
+        # 1 1 1 1 1
+        # 0 0 0 0 0
+        # 0 1 0 1 1
+        self.sim_qr = QrValueTable()
+        self.sim_qr.table[0, 0] = True
+        self.sim_qr.table[0, 1] = False
+        self.sim_qr.table[0, 2] = True
+        self.sim_qr.table[0, 3] = True
+        self.sim_qr.table[0, 4] = False
+        self.sim_qr.table[1, 0] = False
+        self.sim_qr.table[1, 1] = True
+        self.sim_qr.table[1, 2] = True
+        self.sim_qr.table[1, 3] = False
+        self.sim_qr.table[1, 4] = False
+        self.sim_qr.table[2, 0] = True
+        self.sim_qr.table[2, 1] = True
+        self.sim_qr.table[2, 2] = True
+        self.sim_qr.table[2, 3] = True
+        self.sim_qr.table[2, 4] = True
+        self.sim_qr.table[3, 0] = False
+        self.sim_qr.table[3, 1] = False
+        self.sim_qr.table[3, 2] = False
+        self.sim_qr.table[3, 3] = False
+        self.sim_qr.table[3, 4] = False
+        self.sim_qr.table[4, 0] = False
+        self.sim_qr.table[4, 1] = True
+        self.sim_qr.table[4, 2] = False
+        self.sim_qr.table[4, 3] = True
+        self.sim_qr.table[4, 4] = True
+        self.sim_qr.size = 5
+
+    def test_getlinel2r_line0_returns_3vectors(self):
+        scan_qr = ScanQr(self.sim_qr)
+        vectors = scan_qr._get_line_left_to_right(0)
+        self.assertEqual(3, len(vectors))
+
+    def test_getlinel2r_line0_vector0_contents_ok(self):
+        scan_qr = ScanQr(self.sim_qr)
+        vectors = scan_qr._get_line_left_to_right(0)
+        self.assertEqual(1, vectors[0].x_length)
+        self.assertEqual(0, vectors[0].y_length)
+        self.assertEqual(0, vectors[0].position.x)
+        self.assertEqual(0, vectors[0].position.y)
+
+    def test_getlinel2r_line0_vector1_contents_ok(self):
+        scan_qr = ScanQr(self.sim_qr)
+        vectors = scan_qr._get_line_left_to_right(0)
+        self.assertEqual(0, vectors[1].x_length)
+        self.assertEqual(3, vectors[1].y_length)
+        self.assertEqual(2, vectors[1].position.x)
+        self.assertEqual(0, vectors[1].position.y)
+
+    def test_getlinel2r_line0_vector2_contents_ok(self):
+        scan_qr = ScanQr(self.sim_qr)
+        vectors = scan_qr._get_line_left_to_right(0)
+        self.assertEqual(1, vectors[2].x_length)
+        self.assertEqual(0, vectors[2].y_length)
+        self.assertEqual(3, vectors[2].position.x)
+        self.assertEqual(0, vectors[2].position.y)
+
